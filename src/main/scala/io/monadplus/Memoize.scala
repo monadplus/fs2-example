2cbe007081d051a4f71cd06fab9b957f490dd18f
Arnau Abella <arnauabella@gmail.com>
2019-06-19 08:56:27 +0200

    Memoize example

--- src/main/scala/io/monadplus/Memoize.scala
+++ src/main/scala/io/monadplus/Memoize.scala
@@ -2,17 +2,42 @@ package io.monadplus
 
 import cats.implicits._
 import cats.effect._
+import cats.effect.concurrent._
 
 object Memoize {
-  // Ref + Def, (ref.of Option + def Either throwable A
-  def memoize[F[_], A](f: F[A])(implicit F: Async[F]): F[F[A]] =
-    ???
+  def memoize[F[_], A](fa: F[A])(implicit F: Async[F]): F[F[A]] =
+    Ref.of[F, Option[Deferred[F, Either[Throwable, A]]]](none).map { ref =>
+      Deferred.uncancelable[F, Either[Throwable, A]].flatMap { d =>
+         ref.modify {
+           case s @ Some(other) =>
+             s -> other.get
+           case None =>
+             Some(d) -> fa.attempt.flatMap(eta => d.complete(eta).as(eta))
+         }.flatten.rethrow
+      }
+    }
 }
 
 object MemoizeTest extends IOApp {
-  import Memoize._
+
+  def memo[F[_]](implicit F: Async[F]): F[_] =
+    Ref.of[F, Int](0).flatMap { ref =>
+      val fa: F[Unit] = ref.update(_ + 1)
+      Memoize.memoize(fa).flatMap { mem =>
+        mem.replicateA(10)
+      } >> ref.get.flatTap( c => Sync[F].delay(println(s"[Memoized] Count: $c")))
+    }
+
+  def notMemo[F[_]](implicit F: Async[F]): F[_] =
+    Ref.of[F, Int](0).flatMap { ref =>
+      val fa: F[Unit] = ref.update(_ + 1)
+      fa.replicateA(10) >> ref.get.flatTap( c => Sync[F].delay(println(s"[Not memoized] Count: $c")))
+    }
 
   def run(args: List[String]): IO[ExitCode] =
-    IO.pure(ExitCode.Success)
+    for {
+      _ <- memo[IO]
+      _ <- notMemo[IO]
+    } yield ExitCode.Success
 }
 
